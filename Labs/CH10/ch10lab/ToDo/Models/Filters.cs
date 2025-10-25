572960c6ce211311a169b764419879f18aaa7ec4
carson-young18 <Carson_Young@insideranken.org>
2025-10-25 04:35:56 -0500

    ch10lab

---
+++ Labs/CH10/ch10lab/ToDo/Models/Filters.cs
@@ -0,0 +1,35 @@
+﻿namespace ToDoDemo.Models
+{
+    public class Filters
+    {
+        public Filters(string fileterstring)
+        {
+            FilterString = fileterstring ?? "all-all-all";
+            string[] filters = FilterString.Split('-');
+            CategoryId = filters[0];
+            Due = filters[1];
+            StatusId = filters[2];
+        }
+
+        public string FilterString { get; }
+        public string CategoryId { get; }
+        public string Due { get; }
+        public string StatusId { get; }
+        public bool HasCategory => CategoryId.ToLower() != "all";
+        public bool HasDue => Due.ToLower() != "all";
+        public bool HasStatus => StatusId.ToLower() != "all";
+
+        public static Dictionary<string, string> DueFilterValues =>
+            new Dictionary<string, string>
+            {
+                {"future", "Future"},
+                {"today", "Today"},
+                {"past", "Past"}
+            };
+
+        public bool IsPast => Due.ToLower() == "past";
+        public bool IsToday => Due.ToLower() == "today";
+        public bool IsFuture => Due.ToLower() == "future";
+
+    }
+}
