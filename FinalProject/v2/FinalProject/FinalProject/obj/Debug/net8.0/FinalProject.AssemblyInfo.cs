2e4f8d66020d2485e442adcf821acac128e2d4f4
carson-young18 <Carson_Young@insideranken.org>
2025-12-01 11:50:00 -0600

    test migration

--- FinalProject/v2/FinalProject/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/v2/FinalProject/FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ae24b7c96b49e781688032e9ad3eb5c2a575ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414f188ff38cff2e52a11cdf47fe6bd80cdc0f8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
